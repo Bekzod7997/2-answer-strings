99677ed6b4d9086b1c780cc6ac50f12de257204e
Bekzod7997 <138597454+Bekzod7997@users.noreply.github.com>
2023-07-20 18:32:13 +0500

    Add project files.

---
+++ 2 answer strings/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace _2_answer_strings
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string input = Console.ReadLine();
+            string[] words = input.Split(' ');
+
+            string initials = "";
+            for (int i = 0; i < words.Length - 1; i++)
+            {
+                initials += words[i][0] + ". ";
+            }
+            initials += words[words.Length - 1];
+
+            Console.WriteLine(initials);
+
+            Console.WriteLine("Enter a sentence:");
+            string input = Console.ReadLine();
+
+            string[] words = input.Split(' ');
+
+            string longestWord = "";
+
+            foreach (string word in words)
+            {
+                if (word.Length > longestWord.Length)
+                {
+                    longestWord = word;
+                }
+            }
+
+            Console.WriteLine("Longest word: " + longestWord);
+
+        }
+    }
+}
